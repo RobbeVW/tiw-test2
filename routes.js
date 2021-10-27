905d752c091a5cfabda0c914cf224bbc3addde47
Emmerjo <jonathan.emmers@student.pxl.be>
2021-10-27 14:33:21 +0200

    Add multiplication backend functionality

--- routes.js
+++ routes.js
@@ -12,8 +12,11 @@ router.get('/add/:num1/:num2', function (req, res) {
 
 // Provide an /subtract endpoint followed by 2 values that will be added. Result is returned
 // in JSON format
-router.get('/subtract/:num1/:num2', function (req, res) {
-  // TODO
+router.get('/multiply/:num1/:num2', function (req, res) {
+    var num1 = parseInt(req.params.num1)
+    var num2 = parseInt(req.params.num2)
+    var multiplication = (num1 * num2).toString()
+    res.json(multiplication)
   
 })
 
